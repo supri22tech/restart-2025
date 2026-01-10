8e0cdac5c813f11f8e9dcbdf276383eacf6218cb
SUPRIYA.P.V <supriyapv222006@gmail.com>
2026-01-10 23:59:00 +0530

    Reset Day 1 - small improvement

--- day3-profile-card/script.js
+++ day3-profile-card/script.js
@@ -1,5 +1,25 @@
 let count=0;
 function increment(){
     count ++;
-    console.log(count);
+    return count;
+}
+
+let marks = 70;
+
+if (marks >= 50){
+    console.log("Pass");
+}
+else{
+    console.log("Fail");
+}
+
+for (let i = 0; i<=10 ; i++){
+    console.log("step :" + i);
+}
+
+
+i = 10;
+function increments(){
+    i ++;
+    return i;
 }
